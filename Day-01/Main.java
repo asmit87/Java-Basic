53e3caa05df1e2118965d1e3aba798d76087cd9c
asmit87 <asmitmehta0@gmail.com>
2026-06-05 23:57:28 +0530

    Day-01 Code

---
+++ Day-01/Main.java
@@ -0,0 +1,17 @@
+// mene print kiya Namaste Bharat
+
+// public static void main(String[] args) {
+
+//     System.out.println("Namaste Bharat");
+
+// }
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello Bharat");
+        
+    }
+}
+
+
+
