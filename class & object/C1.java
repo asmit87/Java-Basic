0ce40b2799eb130193ce290567b914d531b89d7d
asmit87 <asmitmehta0@gmail.com>
2026-06-08 19:26:01 +0530

    Add student class and C1 main class for student management

---
+++ class & object/C1.java
@@ -0,0 +1,22 @@
+class student {
+    int rollNo;
+    String studentName;
+}
+
+public class C1 {
+    public static void main(String[] args) {
+        student obj1 = new student();
+        obj1.rollNo = 101;
+        obj1.studentName = "John Doe";
+
+        System.out.println("Roll No: " + obj1.rollNo);
+        System.out.println("Student Name: " + obj1.studentName);
+
+        student obj2 = new student();
+        obj2.rollNo = 102;
+        obj2.studentName = "Jane Smith";
+
+        System.out.println("Roll No: " + obj2.rollNo);
+        System.out.println("Student Name: " + obj2.studentName);
+    }
+}
